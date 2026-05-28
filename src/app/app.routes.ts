b435a6342b57d5d42c1b87fddd9a510dcfefd728
Jorge Lino Santiesteban Toca <jorge.santiesteban@etecsa.cu>
2026-05-28 10:06:56 -0500

    refactor: remove landing component and associated template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { dashboardChildRoutes } from './features/dashboard/dashboard.routes';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
+  { path: 'reset-password', loadComponent: () => import('./features/reset-password/reset-password.component').then(m => m.ResetPasswordComponent) },
   {
     path: 'dashboard',
     loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
