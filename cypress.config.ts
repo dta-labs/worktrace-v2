e8eba56819039e42ff26f6932796839aeeea62dd
Jorge Lino Santiesteban Toca <jorge.santiesteban@etecsa.cu>
2026-06-02 18:15:31 -0500

    config: update cypress and package dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+    projectId: "vk83qa",
     e2e: {
         baseUrl: 'http://localhost:4200',
         pageLoadTimeout: 120000,
@@ -8,8 +9,12 @@ export default defineConfig({
         specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
         supportFile: 'cypress/support/e2e.ts',
         fixturesFolder: 'cypress/fixtures',
+        viewportWidth: 1280,
+        viewportHeight: 720,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
     },
+    experimentalInteractiveRunEvents: true,
+    allowCypressEnv: false
 });
